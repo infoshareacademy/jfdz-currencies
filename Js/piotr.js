875e334bff9f4eacd7c59998c19c718a9d8ac88c
pperetko <pperetko@gmail.com>
2016-02-07 18:39:06 +0100

    formularz

--- Js/piotr.js
+++ Js/piotr.js
@@ -1,9 +1,6 @@
 function MenuOnClick(p1) {
     document.getElementById('call-to-action').style.display = 'none';
     document.getElementById('onas').style.display = 'none';
-    //document.getElementById('moje_zwierzeta').style.display = 'none';
-    //document.getElementById('moja_galeria').style.display = 'none';
-    //document.getElementById('moje_podroze').style.display = 'none';
     switch (p1) {
         case 1:
             document.getElementById('call-to-action').style.display = 'block';
@@ -11,15 +8,6 @@ function MenuOnClick(p1) {
         case 2:
             document.getElementById('onas').style.display = 'block';
             break;
-        //case 3:
-        //    document.getElementById('moje_zwierzeta').style.display = 'block';
-        //    break;
-        //case 4:
-        //    document.getElementById('moja_galeria').style.display = 'block';
-        //    break;
-        //case 5:
-        //    document.getElementById('moje_podroze').style.display = 'block';
-        //    break;
 
     }
 }
@@ -27,7 +15,6 @@ function MenuOnClick(p1) {
 
 function ValidateFrom(form){
     var next = true;
-    //Walidacja emaila
     if (next)
     {
         var TestEmail = /^(.[A-Za-z0-9\-]*\w)+@+([A-Za-z0-9\-]*\w)+(\.[A-Za-z]*\w)+$/;
