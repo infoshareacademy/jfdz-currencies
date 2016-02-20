bcde15d1a75f708b77e14386225979fce9b30f3a
krolik <markrolikowski@gmail.com>
2016-02-20 18:21:28 +0100

    Przesuniecie przycisku. Dodanie gry othello (szkic)

--- Js/othello.js
+++ Js/othello.js
@@ -9,11 +9,12 @@ function setupGame(rozmiar) {
 }
 
 function setSpace(rozmiar) {
-    for (var i = 0; i < rozmiar; i++ ) {
-        for (var j = 0; j < rozmiar; j++) {
-            var wiersz = i+1;
-            var kolumna = j+1;
-            if ((i+j)%2 == 0) {
+
+    for (var i=0; i<rozmiar; i++ ) {
+        for (var j=0; j<rozmiar; j++) {
+            var wiersz=i+1;
+            var kolumna=j+1;
+            if ((i+j)%2==0) {
                 appendMsg('<div class="euro" style="background-color: darkgreen;">A</div>');
             } else {
                 appendMsg('<div class="dolar" style="background-color: forestgreen;">B</div>');
