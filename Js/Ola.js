e473debbc66178ba34cc90856186f565984a228d
aleksandra.gorna <aleksandra1>
2016-02-22 09:17:47 +0100

    dodanie klasy na slajder

--- Js/Ola.js
+++ Js/Ola.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
 
-                $(this).animate({'opacity':'1'},500);
+                $(this).animate({'opacity':'1'},1000);
 
             }
 
