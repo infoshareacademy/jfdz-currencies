8b41d6a0fd3c481c94486c7622f3e652cbea7876
aleksandragorna <aleksandra-gorna@o2.pl>
2016-03-06 17:27:58 +0100

    scroll up

--- Js/Ola.js
+++ Js/Ola.js
@@ -57,9 +57,30 @@ $(document).ready(function(){
 
 });
 
+$(document).ready(function() {
+    $('.dodatki').css({marginLeft: -1100});
 
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
 
+        /* Check the location of each desired element */
+        $('.dodatki').each( function(i){
 
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'margin-left':'0'},5000);
+
+            }
+
+        });
+
+    });
+
+});
 
 
 
