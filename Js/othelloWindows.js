39467d3fcc92490f84352692eb9b89897c56b526
krolik78 <markrolikowski@gmail.com>
2016-03-26 15:47:37 +0100

    Przelaczanie pomiedzy ekranami gry

---
+++ Js/othelloWindows.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Marcin on 2016-03-26.
+ */
+function setupScreen() {
+    $(document).ready( function () {
+        $('#rulesScreen').remove();
+        $('#gameScreen').remove();
+    });
+
+}
